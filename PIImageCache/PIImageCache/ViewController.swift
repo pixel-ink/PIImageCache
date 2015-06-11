ca8f0e317e2e9e1bb31f52f964114280f52aab62
pixel-ink <fujiwara@yoshiki.mobi>
2015-06-11 10:07:07 +0900

    動作確認用のView

--- PIImageCache/PIImageCache/ViewController.swift
+++ PIImageCache/PIImageCache/ViewController.swift
@@ -2,16 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  let cache = PIImageCache()
+
+  @IBOutlet var imgView: UIImageView!
+  
+  @IBAction func btnPushed(sender: AnyObject) {
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+  
 }
 
